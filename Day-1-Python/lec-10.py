123048e5f351af3c84f176b83e611e6c50f4d1ad
KPAtDivineInfoverse <kp.divineinfoverse@gmail.com>
2025-02-25 13:43:49 +0530

    25-02-2025-01-43-PM

---
+++ Day-1-Python/lec-10.py
@@ -0,0 +1,11 @@
+a=input("Enter your Name :")
+print(f"My name Is {a}")
+
+x=input("Enter 1 No :")
+y=input("Enter 2 No :")
+
+#aa print be string nu concatination return krse beacuse input() string return krse atle ex 2+5 = 25 krse print(x+y)
+print(x+y)
+
+#aa print x+y no proper  Addition(+) return krse because aapne x and y ne pela int() ma convert kari didha
+print(int(x)+int(y))
---
+++ Day-1-Python/lec-9.py
@@ -0,0 +1,15 @@
+#explicit typecasting
+a="1"
+b="2"
+print(a+b)
+print(int(a)+int(b))
+string="15"
+number=9
+sum=number+int(string)
+print("Sum = ",sum)
+
+#implicit typecasting
+c=3.9
+d=8
+print(c+d)
+#here in implicit typecasting theanswer of c+d is automatically coverts in a float
