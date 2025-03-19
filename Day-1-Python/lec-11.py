1162833bf5054386756e116744cb0b5a30ef3d13
KPAtDivineInfoverse <kp.divineinfoverse@gmail.com>
2025-03-19 12:40:57 +0530

    19-03-2025-12-40-PM

---
+++ Day-1-Python/lec-11.py
@@ -0,0 +1,35 @@
+name="kip"
+print("Hello, "+ name)
+apple_one='he said,"i want to eat".'
+apple="he said,\"i want to eat\"."
+print(apple)
+print(apple_one)
+
+#multiline string
+
+a="""name="kip"
+print("Hello, "+ name)
+apple_one='he said,"i want to eat".'
+apple="he said,\"i want to eat\"."
+print(apple)
+print(apple_one)"""
+
+b='''name="kip"
+print("Hello, "+ name)
+apple_one='he said,"i want to eat".'
+apple="he said,\"i want to eat\"."
+print(apple)
+print(apple_one)'''
+
+print(a)
+print(b)
+
+print(name[0])
+print(name[1])
+
+my_name="Kapish"
+
+for character in my_name:
+    print(character)
+
+
