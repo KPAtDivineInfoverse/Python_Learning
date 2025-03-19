157b8a7f8dbf868d72c2365a175b5315b9d2d062
KPAtDivineInfoverse <kp.divineinfoverse@gmail.com>
2025-03-19 11:40:10 +0530

    19-03-2025-11-39-AM

--- Day-1-Python/lec-10.py
+++ Day-1-Python/lec-10.py
@@ -7,8 +7,8 @@ y=input("Enter 2 No :")
 #aa print be string nu concatination return krse beacuse input() string return krse atle ex 2+5 = 25 krse print(x+y)
 print(x+y)
 # print(x-y)
-print(x*y)
-print(x/y)
+print(int(x)*int(y))
+print(int(x)/int(y))
 
 #aa print x+y no proper  Addition(+) return krse because aapne x and y ne pela int() ma convert kari didha
 print(int(x)+int(y))
