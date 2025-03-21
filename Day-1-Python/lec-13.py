e24e0e68e951cfa2d5ee4cf5d4d19d175581a49c
KPAtDivineInfoverse <kp.divineinfoverse@gmail.com>
2025-03-21 17:54:59 +0530

    21-03-2025-05-54-PM

--- Day-1-Python/lec-13.py
+++ Day-1-Python/lec-13.py
@@ -32,3 +32,38 @@ print(str1.endswith("to",4,10))
 
 str3="He's name is Dan. He is an honest man."
 print(str3.find("is"))
+print(str3.index("Dan"))
+
+str4="WelcomeToPython"
+print(str4.isalnum())
+str4="WelcomeToPython007"
+print(str4.isalnum())
+print(str4.isalpha())
+str4="WelcomeToPython"
+print(str4.isalpha())
+print(str4.islower())
+str4="welcometopython"
+print(str4.islower())
+print(str4.isprintable())
+str4="welcometopython\n"
+print(str4.isprintable())
+
+str5="      " #using spacebar
+print(str5.isspace())
+str5="      " #using Tab
+print(str5.isspace())
+str5="Welcome To Python"
+print(str5.isspace())
+print(str5.istitle())
+str5="Welcome to python"
+print(str5.istitle())
+
+str6="WORLD HEALTH ORGANIZATION"
+print(str6.isupper())
+
+str7="Python Is Good Language"
+print(str7.startswith("Python"))
+print(str7.swapcase())
+
+str8="He's name is Dan"
+print(str8.title())
