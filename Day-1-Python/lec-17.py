3fce553df6e9a85f60c059a2d08203c1f7cb9d8e
KPAtDivineInfoverse <kp.divineinfoverse@gmail.com>
2025-03-31 18:02:36 +0530

    31-03-2025-06-02-PM

---
+++ Day-1-Python/lec-17.py
@@ -0,0 +1,26 @@
+# name = "Kapish"
+
+# for i in name:
+#     print(i)
+#     if( i == 'p'):
+#         print("this is special")
+        
+# colors = ["Red","Green","Blue"]
+
+# for color in colors:
+#     print(color)
+#     for i in color:
+#          print(i)
+         
+# for j in range(5):
+#     print(j)
+    
+# for k in range(5):
+#     print(k+1)
+    
+# for h in range(1,5):
+#     print(h)
+    
+for x in range(1,10,2):
+    print(x)
+    
